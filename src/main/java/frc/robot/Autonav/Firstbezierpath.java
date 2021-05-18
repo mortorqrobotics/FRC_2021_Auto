cb709e3321108e48cfa1a61dc15de81161e3469b
Cameron <meme34903@gmail.com>
2021-05-18 14:29:01 -0700

    refactor + fix

--- src/main/java/frc/robot/Autonav/Firstbezierpath.java
+++ src/main/java/frc/robot/Autonav/Firstbezierpath.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import frc.robot.Drivetrain;
 import frc.robot.Utils.BezierCurve;
-import frc.robot.Utils.BezierDerivative;
 import frc.robot.Utils.Convert;
 
 public class Firstbezierpath {
@@ -38,7 +37,7 @@ public class Firstbezierpath {
 
     public void FirstInit(Drivetrain drive, Trajectory trajectory){
 
-        drive.resetOdometry(new Pose2d(new Translation2d(4.0, 6.86), Rotation2d.fromDegrees(0)));
+        drive.resetOdometry(new Pose2d(new Translation2d(3.9577, 6.8776), Rotation2d.fromDegrees(0)));
         firstEquation = new BezierCurve((t) -> (39.5*Math.pow(t, 0)*Math.pow(1-t, 3)+253.5*Math.pow(t, 1)*Math.pow(1-t, 2)+354.0*Math.pow(t, 2)*Math.pow(1-t, 1)+131.5*Math.pow(t, 3)*Math.pow(1-t, 0)) * 0.1
                                         , (t) -> (-82.0*Math.pow(t, 0)*Math.pow(1-t, 3)-201.0*Math.pow(t, 1)*Math.pow(1-t, 2)-199.5*Math.pow(t, 2)*Math.pow(1-t, 1)-82.0*Math.pow(t, 3)*Math.pow(1-t, 0)) * 0.1
                                         , 9.632);
@@ -90,81 +89,82 @@ public class Firstbezierpath {
 
                 t = distance / firstEquation.distance;
                 changeInX = firstEquation.funcX.apply(t + 0.0001) - firstEquation.funcX.apply(t);
-                slope = -BezierDerivative.derive(firstEquation.funcX, firstEquation.funcY, t);
+                slope = -firstEquation.derive(t);
                 angle = Convert.getDegrees(slope, 1);
-
                 break;
-
             case SECOND_EQUATION:
                 distance -= firstEquation.distance;
-                if (distance > secondEquation.distance)
+                if (distance > secondEquation.distance) {
                     state = State.THIRD_EQUATION;
+                }
                 
                 t = distance / secondEquation.distance;
                 changeInX = secondEquation.funcX.apply(t + 0.0001) - secondEquation.funcX.apply(t);
 
-                slope = -BezierDerivative.derive(secondEquation.funcX, secondEquation.funcY, t);
+                slope = -secondEquation.derive(t);
                 angle =  Convert.getDegrees(slope, 1);
                 break;
-
             case THIRD_EQUATION:
                 distance -= firstEquation.distance + secondEquation.distance;
-                if (distance > thirdEquation.distance) 
+                if (distance > thirdEquation.distance) {
                     state = State.FOURTH_EQUATION;
+                }
 
                 t = distance / thirdEquation.distance;
                 changeInX = thirdEquation.funcX.apply(t + 0.0001) - thirdEquation.funcX.apply(t);
 
-                slope = -BezierDerivative.derive(thirdEquation.funcX, thirdEquation.funcY, t);
+                slope = -thirdEquation.derive(t);
                 angle =  Convert.getDegrees(slope, 1);
                 break;
             case FOURTH_EQUATION:
                 distance -= firstEquation.distance + secondEquation.distance + thirdEquation.distance;
-                if (distance > fourthEquation.distance) 
+                if (distance > fourthEquation.distance) {
                     state = State.FIFTH_EQUATION;
+                }
 
                 t = distance / fourthEquation.distance;
                 changeInX = fourthEquation.funcX.apply(t + 0.0001) - fourthEquation.funcX.apply(t);
 
-                slope = -BezierDerivative.derive(fourthEquation.funcX, fourthEquation.funcY, t);
+                slope = -fourthEquation.derive(t);
                 angle =  Convert.getDegrees(slope, 1);
                 break;
             case FIFTH_EQUATION:
                 distance -= firstEquation.distance + secondEquation.distance + thirdEquation.distance + fourthEquation.distance;
-                if (distance > fifthEquation.distance) 
+                if (distance > fifthEquation.distance) { 
                     state = State.SIXTH_EQUATION;
+                }
 
                 t = distance / fifthEquation.distance;
                 changeInX = fifthEquation.funcX.apply(t + 0.0001) - fifthEquation.funcX.apply(t);
 
-                slope = -BezierDerivative.derive(fifthEquation.funcX, fifthEquation.funcY, t);
+                slope = -fifthEquation.derive(t);
                 angle =  Convert.getDegrees(slope, 1);
                 break;
             case SIXTH_EQUATION:
                 distance -= firstEquation.distance + secondEquation.distance + thirdEquation.distance + fourthEquation.distance + fifthEquation.distance;
-                if (distance > sixthEquation.distance) 
+                if (distance > sixthEquation.distance) { 
                     state = State.SEVENTH_EQUATION;
+                }
 
                 t = distance / sixthEquation.distance;
                 changeInX = sixthEquation.funcX.apply(t + 0.0001) - sixthEquation.funcX.apply(t);
-
-                slope = -BezierDerivative.derive(sixthEquation.funcX, sixthEquation.funcY, t);
+                slope = -sixthEquation.derive(t);
                 angle =  Convert.getDegrees(slope, 1);
                 break;
             case SEVENTH_EQUATION:
                 distance -= firstEquation.distance + secondEquation.distance + thirdEquation.distance + fourthEquation.distance + fifthEquation.distance + sixthEquation.distance;
-                if (distance > seventhEquation.distance) 
+                if (distance > seventhEquation.distance) { 
                     state = State.STOP;
+                }
 
                 t = distance / seventhEquation.distance;
                 changeInX = seventhEquation.funcX.apply(t + 0.0001) - seventhEquation.funcX.apply(t);
-
-                slope = -BezierDerivative.derive(seventhEquation.funcX, thirdEquation.funcY, t);
-                angle =  Convert.getDegrees(slope, 1);
+                slope = -seventhEquation.derive(t);
+                angle = Convert.getDegrees(slope, 1);
                 break;
             default:
                 stopMoving = true;
-                angle =  0.0;
+                angle = 0.0;
                 break;
         }
 
--- src/main/java/frc/robot/Utils/BezierCurve.java
+++ src/main/java/frc/robot/Utils/BezierCurve.java
@@ -6,10 +6,15 @@ public class BezierCurve {
     public DoubleFunction<Double> funcX;
     public DoubleFunction<Double> funcY;
     public double distance;
+    private double DT = 0.001;
 
     public BezierCurve(DoubleFunction<Double> funcX, DoubleFunction<Double> funcY, double distance) {
         this.funcX = funcX;
         this.funcY = funcY;
         this.distance = distance;
+    }
+
+    public double derive(double t) {
+        return (funcY.apply(t + DT) - funcY.apply(t)) / (funcX.apply(t + DT) - funcX.apply(t));
     }
 }
